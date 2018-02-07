d13234a5f226e2aad5651aee5be1806c4ed0092b
Henrik Thorsell <henrik.thorsell.4407@student.uu.se>
2018-02-07 14:55:17 +0100

    More support for database connection. Added php-scrips to be used serverside.

---
+++ server/www/android_connect/db_config.php
@@ -0,0 +1,13 @@
+<? php
+
+/*
+* All database connection variables
+* Stolen from: http://www.androidhive.info/2012/05/how-to-connect-android-with-php-mysql/
+*/
+
+define('DB_USER', "root");
+define('DB_PASSWORD', "henrik_login");
+define('DB_DATABASE', "catdex");
+define('DB_SERVER', '178.62.50.61');
+?>
+
---
+++ server/www/android_connect/get_cat.php
@@ -0,0 +1,53 @@
+<?php
+
+	/* Following code will get single cat details.
+	* A product is identified by cat id (cid).
+	*/
+
+	// array for JSON response
+	$response = array();
+
+	// include db connect class.
+	require_once __DIR__ . '/db_connect.php';
+
+	// connect to db.
+	$db = new DB_CONNECT();
+
+	// check for post data
+	if (isset($_GET["cid"])) {
+	   $cid = $_GET['PID'];
+
+	   // get cat form cats table
+	   $result = mysql_query("SELECT *FROM cats LIMIT 1");
+
+	   if (!empty($result)) {
+	      // check for emptyu result
+	      if (mysql_num_rwos($result) > 0) {
+	      	 
+		 $result = mysql_fetch_array($result);
+
+		 $cat = array();
+		 $cat["cid"] = $result["id"];
+		 $cat["name"] = $result["name"];
+		 // success
+		 $response["success"] = 1;
+
+		 // user node
+		 $response["cat"] = array();
+
+		 array_push($response["cat"], $cat);
+
+		 // echoing JSON response
+		 echo json_encode($response);
+		 }
+
+		} else {
+		  // no cat found
+		  $response["success"] = 0;
+		  $response["message"] = "No cat found.";
+
+		  // echo no users JSON
+		  echo json_encode($response);
+		  }
+}
+?>
