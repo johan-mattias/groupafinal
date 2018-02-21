5e8a562a81b9024a4dc25827a49630380313c04f
Henrik Thorsell <henrik.thorsell.4407@student.uu.se>
2018-02-21 17:54:55 +0100

    Added new solution to adding cats to the DB, the previous one wasn't working. Fixed the get_all_cats_by_tag.php, turned out to be a typo in the code. Begun some work on the main menu.

--- myTESTAPP/app/src/main/java/se/thorsell/catdex/NewCatActivity.java
+++ myTESTAPP/app/src/main/java/se/thorsell/catdex/NewCatActivity.java
@@ -1,15 +1,30 @@
 package se.thorsell.catdex;
 
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -27,8 +42,6 @@ public class NewCatActivity extends Activity {
 
     JSONParser jsonParser = new JSONParser();
     EditText inputName;
-    EditText inputPrice;
-    EditText inputDesc;
 
     // url to create new product
     private static String url_create_cat = "http://178.62.50.61/android_connect/create_cat.php";
@@ -82,35 +95,50 @@ public class NewCatActivity extends Activity {
         protected String doInBackground(String... args) {
             String name = inputName.getText().toString();
 
-            // Building Parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("name", name));
-
-            // getting JSON Object
-            // Note that create product url accepts POST method
-            JSONObject json = jsonParser.makeHttpRequest(url_create_cat,
-                    "POST", params);
-
-            // check log cat fro response
-            Log.d("Create Response", json.toString());
-
-            // check for success tag
+            // This try-catch section a modification of: https://stackoverflow.com/a/42780501
             try {
-                int success = json.getInt(TAG_SUCCESS);
+                // Connect to the server.
+                URL url = new URL("http://178.62.50.61/android_connect/create_cat.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-                if (success == 1) {
-                    // successfully created product
-                    Intent i = new Intent(getApplicationContext(), AllCatsActivity.class);
-                    startActivity(i);
+                // Set variables for the connection.
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                conn.setRequestProperty("Accept","application/json");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
 
-                    // closing this screen
-                    finish();
-                } else {
-                    // failed to create product
-                }
-            } catch (JSONException e) {
+                // Create the JSON object to be sent, and add the name of the new cat to it.
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("name", name);
+                Log.i("JSON", jsonParam.toString());
+
+                // Create a data output stream and write jsonParam.
+                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                os.writeBytes(jsonParam.toString());
+
+                // Flush and close the output stream.
+                os.flush();
+                os.close();
+
+                // Log the response of the server connection.
+                Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                Log.i("MSG" , conn.getResponseMessage());
+
+                // Terminate the connection.
+                conn.disconnect();
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+
+
+            // Move back to main activity.
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+
+            // Closing this screen.
+            finish();
 
             return null;
         }
