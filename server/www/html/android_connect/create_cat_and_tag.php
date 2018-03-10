f09eb361ebf2df8219dae90ef3c265c14944661f
Henrik Thorsell <henrik.thorsell.4407@student.uu.se>
2018-03-10 13:22:41 +0100

    You can now create a cat and add a tag to it. New tags are only created if the entered tag doesn't already exist in the database. The catsTagsMap is also updated to match the cat with it's tag. Started work on displaying images with the cats when filtering by tag. See previous commit for Android files.

---
+++ server/www/html/android_connect/create_cat_and_tag.php
@@ -0,0 +1,35 @@
+<?php
+// This code opens a connection to the database, gets the input value and inserts
+// a new cat record into the database with the input value as the cat name.
+
+// Include the login information stored in the db_config.php file.
+require '../../includes/db_config.php';
+
+// Create connecton using required DEFINEs
+$conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+// Check connection (from w3schools website)
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the JSON struct and decode it.
+$data = json_decode(file_get_contents('php://input'), true);
+
+// Sanitize input before quering the database.
+$data['name'] = mysqli_real_escape_string($conn, $data['name']);
+$data['image'] = mysqli_real_escape_string($conn, $data['image']);
+$data['tag'] = mysqli_real_escape_string($conn, $data['tag']);
+
+// Combine the three statemens of insert tag, insert cat and update the catsTagsMap.
+// TODO This is probably not safe for multiple parallel connections.
+$sqlMulti = "INSERT IGNORE INTO tags (tag) VALUES ('".$data['tag']."'); ";
+$sqlMulti.= "INSERT INTO cats (name, image) VALUES ('".$data['name']."', '".$data['image']."'); ";
+$sqlMulti.= "INSERT INTO catsTagsMap (cat_id, tag_id) SELECT MAX(cats.id), MAX(tags.id) FROM cats, tags; ";
+
+// Run all three statements.
+if(!$conn->multi_query($sqlMulti)){
+  echo "Multi query failed: (" . $conn->errno . ") " .$conn->error;
+}
+$conn->close();
+?>
